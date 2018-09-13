c78a6b84cbacb9ae3b409f82c4809a692e41066c
rayanfahlevi <42663030+rayanfahlevi@users.noreply.github.com>
2018-09-13 08:58:24 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,40 @@
-<?php
-
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+ <?php
+ $batas = 5;
+	function rekursif($i=1){
+	global $batas;
+  echo "*";
+  $i++;
+  	if($i<=$batas){
+    	rekursif($i);
+	}
+	else{
+		echo "<br>";
+		$batas--;
+		if ($batas!=0) {
+			rekursif();
+		}
+	}
 }
-rekursif(5);
+rekursif();
+?>
+
+///////////////////////////////////
+
+<?php
+function rekursif2($a = 1){
+	global $bts2;
+	echo "$a";
+	$a++;
+	if ($a<=$bts2) {
+	rekursif2($a);
+	}
+	else{
+		echo "<br>";
+		$bts2++;
+		if ($bts2 <= 5) {
+			rekursif2();
+		}
+	}
+}
+rekursif2();
 ?>
